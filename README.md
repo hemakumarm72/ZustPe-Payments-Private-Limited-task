d46bea6463c258901ffb223653d3054df2b7995d
unknown <hemakumarm72@gmail.com>
2022-07-03 09:13:59 +0530

    ignore of favicon.icon api

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ install [url]https://nodejs.org/dist/v16.15.0/node-v16.15.0-x64.msi
 
 > > npm install
 > > npm start
+
+> > > http://localhost:8000
--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 8000;
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:8000'],
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+function ignoreFavicon(req, res, next) {
+  if (req.originalUrl.includes('favicon.ico')) {
+    res.status(204).end();
+  }
+  next();
+}
+app.use(ignoreFavicon);
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.status(200).json('zustpe payment  api');
