92106d5e261f9bb2ff625bdcee252b955bcb587a
unknown <hemakumarm72@gmail.com>
2022-07-03 16:34:34 +0530

    secreat is removed

--- src/models/index.js
+++ src/models/index.js
@@ -2,37 +2,30 @@ import Sequelize from 'sequelize';
 import fs from 'fs';
 import path from 'path';
 
-import env from '../../config/env';
+const { env } = process;
 
-const sequelize = new Sequelize(
-  env.database,
-  env.username,
-  env.password,
-  {
-
-    host: env.host,
-    dialect: env.dialect,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-      keepAlive: true,
+const sequelize = new Sequelize(env.database, env.username, env.password, {
+  host: env.host,
+  dialect: env.dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    pool: {
-      max: env.max,
-      min: env.pool.min,
-      acquire: env.pool.acquire,
-      idle: env.pool.idle,
-    },
-
-    // dialect: 'sqlite',
-    // storage: './db.sqlite',
-    // define: {
-    // },
+    keepAlive: true,
+  },
+  pool: {
+    max: env.pool.max,
+    min: env.pool.min,
+    acquire: env.pool.acquire,
+    idle: env.pool.idle,
   },
 
-);
+  // dialect: 'sqlite',
+  // storage: './db.sqlite',
+  // define: {
+  // },
+});
 
 const db = {};
 
@@ -40,13 +33,14 @@ const basename = path.basename(__filename);
 
 fs.readdirSync(path.join(__dirname))
   .filter(
-    (file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js',
+    (file) =>
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
   )
   .forEach((file) => {
     // eslint-disable-next-line
     const model = require(path.join(__dirname, file)).default(
       sequelize,
-      Sequelize.DataTypes,
+      Sequelize.DataTypes
     );
     db[model.name] = model;
   });
